e006c6c323b0d2b55be66c60c71706e75fa070d4
Raziel <juansexd7w7@gmail.com>
2025-12-04 07:44:38 -0500

    diseñp pagina principal

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Juego de Preguntas SENA</title>
+    
+    <link rel="stylesheet" href="assets/css/diseño_index.css" />
+</head>
+<body>
+    <div class="container">
+
+        <img src="assets/img/logo-sena.png" alt="Logo SENA" class="logo-sena">
+
+        <h1>JUEGO DE PREGUNTAS</h1>
+            
+        <div class="card">
+
+            <button class="btn" onclick="location.href='../views/login.php'">iniciar sesion</button>
+            <button class="btn" onclick="location.href='../views/logout.php'">registrarse</button>
+        </div>
+    </div>
+</body>
+</html>
